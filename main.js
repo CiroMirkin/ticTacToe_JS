565af2bb9853b9d4327b38dedfaf161a4c0b575c
C.mirkin <ciromirkin@gmail.com>
2012-01-01 00:42:03 -0300

    agregado: cada jugador puede poner tres fichas sobre el tablero

--- main.js
+++ main.js
@@ -13,6 +13,45 @@ const userWinsWhen = [
     'a3 b2 c1'
 ]
 
+const indexCards = {
+    blackPlayer: [
+        '<div id="black-3" class="indexCard indexCard--black"><span class="circle"></span></div>',
+        '<div id="black-2" class="indexCard indexCard--black"><span class="circle"></span></div>',
+        '<div id="black-1" class="indexCard indexCard--black"><span class="circle"></span></div>'
+    ],
+    whitePlayer: [
+        '<div id="white-3" class="indexCard indexCard--white"><span class="circle"></span></div>',
+        '<div id="white-2" class="indexCard indexCard--white"><span class="circle"></span></div>',
+        '<div id="white-1" class="indexCard indexCard--white"><span class="circle"></span></div>'
+    ]
+}
+
+let playWhite = true
+
+const getIndexCard = () => {
+    let indexCard = ''
+
+    if (playWhite) {
+        indexCard = indexCards.whitePlayer.at(-1)
+        indexCards.whitePlayer.pop()
+        playWhite = false
+        
+        return indexCard 
+    }
+
+    indexCard = indexCards.blackPlayer.at(-1)
+    indexCards.blackPlayer.pop()
+    playWhite = true
+
+    return indexCard 
+}
+
+let areThereIndexCards = true
+
 tableGame.addEventListener('click', (e) => {
-    
+    if (e.target.classList[0] == 'row__cell' && areThereIndexCards) {
+        const cell = document.getElementById(e.target.id)
+        cell.innerHTML = getIndexCard() || ''
+        if(!cell.innerHTML) areThereIndexCards = false
+    }
 })
